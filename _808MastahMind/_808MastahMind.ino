3c79f8cd182c3303ac084a5a5a07dfff2494e5a5
rkam16 <rkam16@punahou.edu>
2014-12-19 15:30:38 -1000

    Final Work Day of the Year
    I just did my guess code formula and researched how to move up the
    board with each guess.

--- _808MastahMind/_808MastahMind.ino
+++ _808MastahMind/_808MastahMind.ino
@@ -53,3 +53,23 @@ void loop()
     if(Button_A&&Button_B){ //reset the game, have to hit both buttons
       reset=true;
       }
+    
+    if (Button_A) {
+    byte correct = 0;  
+    byte correctColorOnly = 0;
+    byte position = 4;
+    boolean secretCodeMatched[4],guessCodeMatched[4];
+    for (int i=0;i<4;i++){ 
+    guessCodeMatched[i]=false;
+    secretCodeMatched[i]=false;
+    }
+    for(int i=0; i<4; i++){ 
+        guessCode[i]=ReadPx(i,guessY);//read the Player's gu7ess into an array
+      }
+      for (int i=0; i<4; i++){ //Count guesses that are correct in color and position
+        if (guessCode[i]==secretCode[i]){
+        secretCodeMatched[i]=true;
+        guessCodeMatched[i]=true;  //so it don't  count them again
+        correct++;
+      }
+     } 
